639878343262122f55ece186c097acd489457eea
shionlau <shionlau@gmail.com>
2016-08-07 18:24:06 -0400

    clean code, add comments

--- simulation/genRndScene.m
+++ simulation/genRndScene.m
@@ -1,5 +1,13 @@
 function genRndScene()
 %%% randomly generate scenarios for simulation
+% Chang Liu 8/6/16
+% parameters to tune:
+% s_pos: starting position of the human
+% shape_set: the set of obstacles shapes
+% obs_num: total number of obstacles
+% tar_num: total number of targets
+% max_spd_set: the maximum speed of the human between a pair of obstacles
+
 clc;
 clear all;
 
@@ -12,21 +20,22 @@ s_pos = [5;5]; % starting position of the human
 % {'r',[w,h]}: rectangle with horizontal size a (width) and vertical size b
 % (height).
 % {'c',a}: circle with radius a
-
 shape_set = {{'r',[5,20]},{'r',[20,5]},{'r',[5,30]},{'c',5}}; 
 
 % generate obstacle positions
 obs_num = 5; % specifiy total number of obstacles
-obs_set = cell(obs_num,1);
+obs_set = cell(obs_num,1); % save info of obstacles
 count = 0; % counter for obstacle
 
 while (count < obs_num)
-    shape_idx = randi(length(shape_set),1); % randomly choose a shape    
+    % randomly generate obstacles until we have the numble of obstacle is obs_num
+    shape_idx = randi(length(shape_set),1); % randomly choose a shape
     tmp_shape = shape_set{shape_idx};
     
-    tmp_pos = [randi([fld_cor(1),fld_cor(3)],1);randi([fld_cor(2),fld_cor(4)],1)]; % find its position
+    % generate position of an obstacle
+    tmp_pos = [randi([fld_cor(1),fld_cor(3)],1);randi([fld_cor(2),fld_cor(4)],1)]; 
     if strcmp(tmp_shape{1},'c')        
-    % for circle, the generated position is its center
+    % for a circle, the generated position is its center
         radius = tmp_shape{2};
         
         % keep this obstacle if it falls within the field and do not
@@ -84,11 +93,11 @@ obs_margin = 3; % the target should be away from the boundaries of obstacles by 
 
 % specify target number
 tar_num = 5; 
-tar_cnt = 1;
-tar_pos = zeros(2,tar_num);
+tar_cnt = 1; % counter for target
+tar_pos = zeros(2,tar_num); % save targets' positions
 
 while (tar_cnt <= tar_num)
-    re_gen = false; % if true, the generated point is inside an obstacle and new regeneration
+    re_gen = false; % if true, the generated point is inside an obstacle and new regeneration is needed
     
     % generate target position
     tmp_pos = [tar_area(3)-tar_area(1);tar_area(4)-tar_area(2)].*...
@@ -154,26 +163,30 @@ for ii = 1:size(tar_pos,2)
     plot(tar_pos(1,ii),tar_pos(2,ii),'r*','MarkerSize',10)
 end
 %}
+
+
 %% find feasible path between targets
 %
-% RRT* 
+% this code usese RRT* to find a path
 hold on
 addpath('rrtstar')
-% plot(s_pos(1),s_pos(2),'g^','MarkerSize',10,'MarkerFaceColor','g');
 
-max_spd = 1; % human speed
+max_spd_set = [1,2,1,3,2]; % human speed for each section
 dt = 0.5;
-max_step = max_spd*dt;
 des_pos = [s_pos,tar_pos]; % human destinations, including his starting position and all target positions
 max_iter = 9e3;  % number of points to sample
 rand_seed = 30; 
 
-fld = struct('fld_cor',fld_cor,'obs_set',{obs_set},'g_pos',zeros(2,1),...
-    's_pos',zeros(2,1),'obs_margin',obs_margin);
+% draw starting position of the human
+plot(s_pos(1),s_pos(2),'g^','MarkerSize',10,'MarkerFaceColor','g');
+
 traj = [];
 for kk = 1:size(des_pos,2)-1
-    fld.s_pos = des_pos(:,kk);
-    fld.g_pos = des_pos(:,kk+1);
+    fld = struct('fld_cor',fld_cor,'obs_set',{obs_set},'g_pos',des_pos(:,kk+1),...
+    's_pos',des_pos(:,kk),'obs_margin',obs_margin);
+    
+    max_spd = max_spd_set(kk);
+    max_step = max_spd*dt;
     
     display(sprintf('max_iter %d',max_iter))
     display(sprintf('rand_seed %d',rand_seed))
@@ -181,6 +194,7 @@ for kk = 1:size(des_pos,2)-1
     is_demo = 0;
     variant = 'Simple2D'; % this class is used for RRT* for a holonomic vehicle
     
+    % find a path using RRT*
     result = pathFinder(fld,max_step,max_iter,is_demo,rand_seed,variant);
     traj = [traj,result.opt_path];
 end
@@ -188,161 +202,12 @@ end
 plot(traj(1,:),traj(2,:),'r')
 
 %}
-
-% MPC
-%{
-% use MPC to find feasible path
-h_max_spd = 1; % human speed
-dt = 0.5;
-des_pos = [s_pos,tar_pos]; % human destinations, including his starting position and all target positions
-hor = 50;
-rect_num = 0;
-
-% find the number of rectangles
-for jj = 1:obs_num 
-    tmp_shape = obs_set{jj};
-    if strcmp(tmp_shape{1},'r')
-        rect_num = rect_num+1;
-    end
 end
 
-x = sdpvar(2,hor+1); % [x;y] coordinate of human
-u = sdpvar(2,hor); % [vx;vy]
-lambda = sdpvar(rect_num,4,hor); % dummy variables
-
-% constraints
-for ii = 1:hor
-    % kinematics
-    constr0 = [x(:,ii+1) == x(:,ii)+u(:,ii)*dt];
-    
-    % collision avoidance
-    rect_cnt = 1; % counter for rectangular obstacles
-    
-    % x > xmax+obs_margin || x < xmin-obs_margin || y > ymax+obs_margin || y < ymin-obs_margin  
-    for jj = 1:obs_num %
-        tmp_shape = obs_set{jj};
-        if strcmp(tmp_shape{1},'c')
-            % for circle, the generated position is its center
-            radius = tmp_shape{2};
-            constr0 = [constr0, sum((x(:,ii+1)-tmp_shape{3}).^2) <= (radius+obs_margin)^2];
-        elseif strcmp(tmp_shape{1},'r')
-            % for rectangle, the generated position is its lower-left coordinate
-            w = tmp_shape{2}(1);
-            h = tmp_shape{2}(2);
-            ll_cor = tmp_shape{3}; % lower-left coordinate
-            xmin = ll_cor(1);
-            xmax = ll_cor(1)+w;
-            ymin = ll_cor(2);
-            ymax = ll_cor(2)+h;
-            constr0 = [constr0, lambda(rect_cnt,:,ii)*[xmax+obs_margin-x(1,ii+1);...
-                x(1,ii+1)-xmin+obs_margin;ymax+obs_margin-x(2,ii+1);...
-                x(2,ii+1)-ymin+obs_margin] <= 0];
-            rect_cnt = rect_cnt+1;
-        end
-    end
-    
-    constr0 = [constr0,sum(squeeze(lambda(:,:,ii)),2) == ones(size(lambda,1),1)];
-end
-
-% bounds on state and input
-constr0 = [constr0, repmat(fld_cor(1:2)',1,size(x,2)) <= x <= repmat(fld_cor(3:4)',1,size(x,2))];
-constr0 = [constr0, sum(u.^2,1) <= h_max_spd^2];
-constr0 = [constr0, lambda >= 0];
-
-% solve for each pair of start and goal position
-for ii = 1%:size(des_pos,2)-1  
-    % initial condition
-    constr = [constr0,x(:,1) == des_pos(:,ii)];
-    
-    % initial guess
-    dx = (des_pos(:,ii+1)-des_pos(:,ii))/hor;
-    guess_x = [des_pos(1,ii):dx(1):des_pos(1,ii+1);des_pos(2,ii):dx(2):des_pos(2,ii+1)];
-    guess_u = guess_x(:,2:end)-guess_x(:,1:end-1);
-    
-    % obj
-    obj = sum(sum(([x(:,2:end),des_pos(:,ii+1)]-x(:,1:end)).^2,1))+...
-        sum(sum(u.^2,1));
-
-    % solve MPC
-    % initial guess
-
-    if ~isempty(guess_x)
-        assign(x,guess_x); % assign initial solution
-        assign(u,guess_u); % assign initial input
-    end
-
-%     opt = sdpsettings('solver','fmincon','fmincon.Algorithm','interior-point','usex0',1,'debug',0,'verbose',1);    
-    opt = sdpsettings('solver','ipopt','usex0',1,'debug',1,'verbose',1);    
-    
-    % solve nlp
-    tic
-    sol = optimize(constr,obj,opt);
-    elapse_time = toc
-    
-    optx = value(x);
-end
-
-plot(optx(1,:),optx(2,:))
-
-%}
-
-% find path to bypass rectangle
-%{
-h_spd = 1; % human speed
-dt = 0.5;
-des_pos = [s_pos;tar_pos]; % human destinations, including his starting position and all target positions
-waypts = s_pos;% waypoints
-for ii = 1:size(des_pos,2)-1    
-    tmp_s = des_pos(:,ii); % start
-    tmp_g = des_pos(:,ii+1); % end
-    cur_waypt = tmp_s;
-    ang = calAngle(tmp_g-tmp_s);
-    
-    % find the next way point until human arrives at the goal
-    while (norm(cur_waypt-tmp_g) >= v*dt)
-        next_waypt = cur_waypt+v*[cos(ang);sin(ang)]*dt;
-        for jj = 1:obs_num % check if the generated point is in an obstacle
-            tmp_shape = obs_set{jj};
-            if strcmp(tmp_shape{1},'c')
-                % for circle, the generated position is its center
-                radius = tmp_shape{2};
-                if norm(next_waypt-tmp_shape{3}) <= radius+obs_margin
-                    tmp_waypts = bypassCircle;
-                    
-                end
-            elseif strcmp(tmp_shape{1},'r')
-                % for rectangle, the generated position is its lower-left coordinate
-                w = tmp_shape{2}(1);
-                h = tmp_shape{2}(2);
-                ll_cor = tmp_shape{3}; % lower-left coordinate
-                if (next_waypt(1) >= ll_cor(1)-obs_margin) && (next_waypt(2) >= ll_cor(2)-obs_margin) && ...
-                        (next_waypt(1) <= ll_cor(3)+obs_margin) && (next_waypt(2) <= ll_cor(4)+obs_margin)
-                    re_gen = true;
-                    break
-                end
-            end
-        end
-    end
-end
-%}
-end
-
-% function [way_pt] = findWayPoint()
-% % if a line connecting two waypoints collide with an obstacle, define a new
-% % way point to by pass the obstacle
-% 
-% 
-% end
-% 
-% function [is_collide,col_obs,col_pt] = collisionChecker()
-% % check whether the staightline between two waypoints intersect with any
-% % obstacles. If yes, find the colliding obstacle and the point of contact
-% 
-% 
-% end
 
 function is_colliding = collisionCheck(shape1, shape2)
 % check if two shapes collide
+
 is_colliding = false;
 
 if strcmp(shape1{1},'r')
